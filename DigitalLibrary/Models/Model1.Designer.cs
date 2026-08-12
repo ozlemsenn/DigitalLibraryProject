fa82df87f0811d8894cb0b427a4cbe95e2c50907
ozlemsen <ozlemsenn871@gmail.com>
2026-08-12 20:08:08 +0300

    tasarımsal değişiklikler yapıldı

--- DigitalLibrary/Models/Model1.Designer.cs
+++ DigitalLibrary/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\pc\OneDrive\Desktop\staj\DigitalLibrary\DigitalLibrary\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\pc\OneDrive\Desktop\staj2\DigitalLibrary\DigitalLibrary\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- DigitalLibrary/Models/Users.cs
+++ DigitalLibrary/Models/Users.cs
@@ -24,6 +24,8 @@ namespace DigitalLibrary.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
+        public string Password { get; set; }
+        public Nullable<bool> IsActive { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Documents> Documents { get; set; }
